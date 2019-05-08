49f4e60fb51a7ac21a1868af217b2e65132fceb9
DullDull <yeop0422@naver.com>
2019-05-08 17:17:36 +0900

    [Engine] DispatcherSelector 작업

--- GameServer/NoobEngine/DispatcherSelector.h
+++ GameServer/NoobEngine/DispatcherSelector.h
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <NoobBasic\Singletone.hpp>
+#include "Dispatcher.h"
+
+namespace Noob
+{
+
+class SingleThreadDispatcher : public Singletone< SingleThreadDispatcher >
+{
+public:
+	SingleThreadDispatcher();
+	~SingleThreadDispatcher();
+	Dispatcher* Get(){ m_dispatcher; }
+private:
+	Dispatcher* m_dispatcher;
+};
+
+class RRDispatcher : public Singletone< RRDispatcher >
+{
+public:
+	RRDispatcher();
+	~RRDispatcher();
+	Dispatcher* Get();
+
+private:
+	::std::vector<Dispatcher*> m_dispatchers;
+	size_t m_index;
+	size_t m_coreCnt;
+};
+
+}
