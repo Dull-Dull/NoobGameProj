f091796b4f751f1e602e96a3dbf1900b824ef8e1
DullDull <yeop0422@naver.com>
2019-06-05 02:03:59 +0900

    [unittest] 유닛테스트 프로젝트 첫커밋

---
+++ GameServer/NoobUnitTest/TestPck/PckRegister.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include<NoobNetwork/PacketRegister.h>
+#include"Pck.h"
+
+REGISTER_PACKET( TestPck1 );
+REGISTER_PACKET( TestPck2 );
---
+++ GameServer/NoobUnitTest/pch.h
@@ -0,0 +1,12 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <NoobBasic/IncAll.h>
+#include <NoobNetwork/IncAll.h>
+#include <NoobEngine/IUser.h>
+#include <NoobEngine/TcpSessionAcceptor.h>
+#include <NoobEngine/TcpSessionConnector.h>
+#include <NoobEngine/PacketDispatcher.h>
+#include <NoobEngine/DispatcherSelector.h>
+
+#endif //PCH_H
